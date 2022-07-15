9990ce71bcf313184b6da747599220241b8bb104
fikradev <sachel.campbell@gmail.com>
2022-07-14 21:38:58 -0500

    added all-listings route

--- server.js
+++ server.js
@@ -1,26 +1,30 @@
-const express = require('express');
-const listingroutes = require('./routes/listingroutes');
-require('dotenv').config();
-
+const express = require("express");
+const mongoose = require("mongoose");
+const listingroutes = require("./routes/listingroutes");
+require("dotenv").config();
 
 //initialize app
 const app = express();
 
+//read in .env data
+PORT = process.env.PORT;
+DBCREDS = process.env.DB_CREDS
+
+//db connection
+const dbUrl = DBCREDS 
+
+mongoose.connect(dbUrl, { useNewUrlParser:true, useUnifiedTopology: true })
+.then((result) => app.listen(PORT))
+.catch((err) => console.log(err));
+
 //setup view engine & views folder
-app.set('view engine', 'ejs')
-app.set('views', 'views')
+app.set("view engine", "ejs");
+app.set("views", "views");
 
 //for static files
-app.use(express.static('public'));
-app.use(listingroutes); 
-
-//read in .env data
-PORT = process.env.PORT
+app.use(express.static("public"));
+app.use(listingroutes);
 
 
 
 
-//listen for requests
-app.listen(PORT, ()=>{
-    console.log("Listening on ....")
-})
