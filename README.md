2c2bd9a7674a625b2e96dd84e8057a472629dbf5
fikradev <sachel.campbell@gmail.com>
2022-07-30 19:30:10 -0500

    added delete functionality

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # nodejs-listing-app
 Real estate listing app built with node .js and MongoDB
+
+1. Install dependencies; express, ejs, mongoose, ejs, dotenv, cors.
+
+2. Create server.js file, import required modules, iniitalize app, setup views and view engine.
+
+3. Create database and setup database connection in server.js.
--- routes/listingroutes.js
+++ routes/listingroutes.js
@@ -1,59 +1,71 @@
-const express = require('express');
-const Proplist = require('../model/listing')
+const express = require("express");
+const Proplist = require("../model/listing");
+var ObjectId = require("mongoose").ObjectId;
 
-const router  = express.Router();
+const router = express.Router();
 
 //Get Routes
 
-router.get('/',(req, res)=>{
-    res.render('index', {title:'Yaad Listing | Home'})
-})
+router.get("/", (req, res) => {
+  res.render("index", { title: "Yaad Listing | Home" });
+});
 
-router.get('/all-listings', (req, res)=>{
-    Proplist.find()
-    .then((result)=>{
-        res.render('all-listings', {title:'Yaad Listing | All-Listings', home: result})
+router.get("/all-listings", (req, res) => {
+  Proplist.find()
+    .then((result) => {
+      res.render("all-listings", {
+        title: "Yaad Listing | All-Listings",
+        home: result,
+      });
     })
-    .catch((err) => { console.log(err)});
-})
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
-router.get('/details', (req, res)=>{
-    res.render('details', {title:'Yaad Listing | Details'})
-})
+router.get("/details", (req, res) => {
+  res.render("details", { title: "Yaad Listing | Details" });
+});
 
-router.get('/add-listing', (req, res)=>{
-    res.render('add-listing', {title:'Yaad Listing | Add a Listing'})
-})
+router.get("/add-listing", (req, res) => {
+  res.render("add-listing", { title: "Yaad Listing | Add a Listing" });
+});
 
-router.get("/details/:id", (req, res)=>{
-    const id = req.params.id;
+router.get("/details/:id", (req, res) => {
+  const id = req.params.id;
 
-    Proplist.findById(id)
-    .then((result)=>{
-        res.render('details', {result, title:'Yaad | Details Page'})
+  Proplist.findById(id)
+    .then((result) => {
+      res.render("details", { result, title: "Yaad | Details Page" });
     })
-    .catch((err)=>{
-        console.log(err)
-    })
-})
-
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 //Post Routes
 
-router.post('/add-listing', async (req, res)=>{
-    const listing = new Proplist(req.body);
+router.post("/add-listing", async (req, res) => {
+  const listing = new Proplist(req.body);
 
-    listing.save()
-    .then((result)=>{
-        res.redirect('/')
+  listing
+    .save()
+    .then((result) => {
+      res.redirect("/");
     })
-    .catch((err) =>{
-        console.log(err)
-    })  
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
-
-
+router.delete('/delete-listing/:id', (req, res)=>{
+  Proplist.deleteOne({_id: req.params.id})
+  .then(()=>{res.status(200).json({message: 'Deleted!!'})})
+  .then(res.redirect('/'))
+  .catch((error)=>{
+    res.status(400).json({error: error})
+  })
+})
 
 //export routes
 module.exports = router;
